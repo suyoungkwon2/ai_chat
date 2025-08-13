b3837023bc187aa4cc598c29b3525eed8a2057e5
suyoungkwon2 <suyoungkwon77@gmail.com>
2025-08-14 01:15:04 +0900

    ui: side bar test buttons hide (fixed)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -83,6 +83,12 @@ export default function Sidebar() {
           <button className="btn btn--home" onClick={handleHomeClick}>
             <span className="icon">🏠</span> <span className="text">Home</span>
           </button>
+          {/* <button className="btn" onClick={() => resetUserRegistration()} title="Reset User" disabled>
+            <span className="icon">🔄</span>
+          </button>
+          <button className="btn" onClick={() => resetAdViews()} title="Reset Ads" disabled>
+            <span className="icon">📺</span>
+          </button> */}
         </div>
 
         <div className="sidebar__section">
